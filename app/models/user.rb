4a8ebbc4c2c803e217d0351e438f9c5662fda85e
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-06-06 15:24:44 +0200

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_one_attached :photo
+    has_one_attached :cover_image
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 12 }
+    validates :fullname, presence: true, length: { minimum: 5, maximum: 20 }
+    has_many :opinions, foreign_key: 'author_id', dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :followings, class_name: 'Following', foreign_key: 'follower_id'
+    has_many :inverse_followings, class_name: 'Following', foreign_key: 'followed_id'
+    has_many :follows, through: :followings, source: :followed
+    has_many :followers, through: :inverse_followings, source: :follower
+    
+
+end
