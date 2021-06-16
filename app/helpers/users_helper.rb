2689bf58d3754ad5462c512cfe7b8822aa9f9b47
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-06-16 15:31:05 +0200

    Add img_src and show_follow? methods to users_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,20 @@
 module UsersHelper
-  def followed_by(user)
-    User.followed_by(Current.user,user).first.full_name if User.followed_by(Current.user,user).first 
+  def followed_by(user,limit=nil)
+    if User.followed_by(Current.user,user).first
+        return User.followed_by(Current.user,user).first.full_name if limit == 1
+        return User.followed_by(Current.user,user) 
+    end
+  end
+
+  def img_src(user)
+    if user.photo.attached?
+      url_for(user.photo)
+    else
+      "https://picsum.photos/70/70"
+    end
+  end
+
+  def show_follow?(user) 
+    Current.user != user && !Current.user.follows.include?(user)
   end
 end
