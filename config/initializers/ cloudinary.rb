3d3d407a0f51d83124a00c39da6bafd011fdd89b
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-06-18 11:29:18 +0200

    update

---
+++ config/initializers/ cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'bishorails'
+  config.api_key = '778394599477445'
+  config.api_secret = 'NNMUvxm1xKiS04eXECs0exsPYVM'
+  config.secure = true
+  config.cdn_subdomain = true
+end
