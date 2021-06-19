e56921a24763df0e9dce2deee3c0dc166696d941
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-06-19 15:12:56 +0200

    Add delete opinion feature

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,4 +1,6 @@
 class OpinionsController < ApplicationController
+  before_action :set_opinion, only: %i[show edit update destroy]
+
   def new
     @opinion = Opinion.new
     @opinions = Opinion.order_by_most_recent
@@ -8,13 +10,23 @@ class OpinionsController < ApplicationController
     @opinion = Current.user.opinions.build(opinion_params)
     if @opinion.save
       flash[:notice] = 'Opinion was created successfully'
-      redirect_to root_path
+      redirect_back(fallback_location: root_path)
     else
       flash.now[:alert] = 'Something went wrong...'
     end
   end
 
+  def destroy
+    @opinion.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_opinion
+    @opinion = Opinion.find(params[:id])
+  end
 
   def opinion_params
     params.require(:opinion).permit(:text)
